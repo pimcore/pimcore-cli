4bb30ba1636b32f6505fce497af364256b5b2b91
Mathias Geat <mathias.geat@elements.at>
2017-04-14 10:26:45 +0200

    Bootstrap PHAR CLI application (CLI script, Application class, box config)

---
+++ bin/pimcore.php
@@ -0,0 +1,38 @@
+#!/usr/bin/env php
+<?php
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Enterprise License (PEL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ * @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ * @license    http://www.pimcore.org/license     GPLv3 and PEL
+ */
+
+// CLI has no memory/time limits
+use Pimcore\Cli\Console\Application;
+
+// only run in PHP >= 7
+$requiredVersion = '7.0';
+if (version_compare(PHP_VERSION, $requiredVersion, '<')) {
+    file_put_contents('php://stderr', sprintf(
+        "Pimcore CLI Tools require PHP 7.0 version or higher and your system has\n".
+        "PHP %s version installed.\n\n".
+        PHP_VERSION
+    ));
+
+    exit(1);
+}
+
+@ini_set('memory_limit', -1);
+@ini_set('max_execution_time', -1);
+@ini_set('max_input_time', -1);
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$application = new Application('Pimcore CLI Tools');
+$application->run();
---
+++ src/Cli/Console/Application.php
@@ -0,0 +1,130 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Enterprise License (PEL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ * @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ * @license    http://www.pimcore.org/license     GPLv3 and PEL
+ */
+
+namespace Pimcore\Cli\Console;
+
+use Symfony\Component\Console\Application as BaseApplication;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\ProcessBuilder;
+
+class Application extends BaseApplication
+{
+    /**
+     * @param string $name
+     */
+    public function __construct($name = 'UNKNOWN')
+    {
+        parent::__construct($name, $this->fetchVersion());
+    }
+
+    /**
+     * Get current version from git describe if not in phar context
+     *
+     * @param bool $readFromPhar
+     *
+     * @return string
+     */
+    public function fetchVersion(bool $readFromPhar = false): string
+    {
+        $packageVersion  = '@package_version@';
+
+        if ($this->isPhar()) {
+            if ($readFromPhar) {
+                return $this->getPharVersion(true);
+            }
+        } else {
+            $gitDir = __DIR__ . '/../../../.git';
+
+            if (file_exists($gitDir)) {
+                $process = new Process(sprintf('git describe --tags'));
+                $process->setEnv([
+                    'GIT_DIR' => $gitDir
+                ]);
+
+                $process->run();
+                if ($process->isSuccessful()) {
+                    $packageVersion = $process->getOutput();
+                }
+            }
+        }
+
+        return $packageVersion;
+    }
+
+    /**
+     * Check if we're running in phar mode
+     *
+     * @return bool
+     */
+    public function isPhar(): bool
+    {
+        $pharPath = \Phar::running();
+
+        return !empty($pharPath);
+    }
+
+    /**
+     * @param bool $spawnProcess
+     *
+     * @return string
+     */
+    public function getPharVersion(bool $spawnProcess = false): string
+    {
+        if (!$this->isPhar()) {
+            throw new \RuntimeException('Can only be called from a PHAR file.');
+        }
+
+        if (!$spawnProcess) {
+            return $this->getVersion();
+        } else {
+            $process = $this->runPharCommand('--version');
+            if (!$process->isSuccessful()) {
+                throw new \RuntimeException($process->getErrorOutput());
+            }
+
+            $output  = trim($process->getOutput());
+            $matches = [];
+
+            if (preg_match('/version (v.*)$/', $output, $matches)) {
+                return $matches[1];
+            } else {
+                return 'UNKNOWN';
+            }
+        }
+    }
+
+    /**
+     * @param string $command
+     * @param array $args
+     *
+     * @return Process
+     */
+    public function runPharCommand(string $command, array $args = []): Process
+    {
+        if (!$this->isPhar()) {
+            throw new \RuntimeException('Can only be called from a PHAR file.');
+        }
+
+        $pharPath = \Phar::running(false);
+
+        $processArgs = array_merge([$pharPath, $command], $args);
+        $process     = (new ProcessBuilder($processArgs))->getProcess();
+        $process->run();
+
+        return $process;
+    }
+}
