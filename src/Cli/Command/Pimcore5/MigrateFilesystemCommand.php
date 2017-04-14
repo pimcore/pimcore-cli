ec7b84429d0a3145a093f928a1600edf5578e71b
Mathias Geat <mathias.geat@elements.at>
2017-04-14 21:14:18 +0200

    WIP: continue implementing MigrateFilesystemCommand

--- src/Cli/Command/Pimcore5/MigrateFilesystemCommand.php
+++ src/Cli/Command/Pimcore5/MigrateFilesystemCommand.php
@@ -17,20 +17,30 @@ declare(strict_types=1);
 
 namespace Pimcore\Cli\Command\Pimcore5;
 
-use Pimcore\Cli\Console\Style\PimcoreStyle;
+use Pimcore\Cli\Command\AbstractCommand;
 use Pimcore\Cli\Console\Style\RequirementsFormatter;
 use Pimcore\Cli\Console\Style\VersionFormatter;
+use Pimcore\Cli\Filesystem\DryRunFilesystem;
 use Pimcore\Cli\Pimcore5\Pimcore5Requirements;
 use Pimcore\Cli\Util\VersionReader;
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Filesystem\Filesystem;
 
-class MigrateFilesystemCommand extends Command
+class MigrateFilesystemCommand extends AbstractCommand
 {
+    /**
+     * @var Filesystem
+     */
+    protected $fs;
+
+    /**
+     * @var string
+     */
+    protected $path;
+
     protected function configure()
     {
         $this->setName('pimcore5:migrate-filesystem');
@@ -51,10 +61,10 @@ class MigrateFilesystemCommand extends Command
             );
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $io = new PimcoreStyle($input, $output);
-        $fs = new Filesystem();
+        $io = $this->io;
+        $fs = $this->fs = new DryRunFilesystem($io, $this->isDryRun());
 
         $path = $input->getArgument('path');
         if (!$fs->exists($path)) {
@@ -63,10 +73,10 @@ class MigrateFilesystemCommand extends Command
             return 1;
         }
 
-        $path = realpath($path);
+        $this->path = $path = realpath($path);
 
         $title = sprintf('Migrating installation %s', $path);
-        if ($this->isDryRun($input)) {
+        if ($this->isDryRun()) {
             $title .= ' (DRY-RUN)';
         }
 
@@ -80,30 +90,95 @@ class MigrateFilesystemCommand extends Command
 
         if (!$input->getOption('no-check-version')) {
             try {
-                $this->checkVersionPrerequisites($io, $versionReader);
-                $io->success('Pimcore version prerequisites match');
+                $this->checkVersionPrerequisites($versionReader);
             } catch (\Exception $e) {
-                $io->error($e->getMessage());
-
-                $code = (int)$e->getCode();
-                if ($code > 0) {
-                    return $code;
-                }
-
-                return 2;
+                return $this->handleException($e, 2);
             }
         }
 
         if (!$input->getOption('no-check-requirements')) {
-            $requirements = $this->checkPimcoreRequirements($io);
-            if (!$requirements) {
-                // requirements formatter already printed errors
-                return 3;
+            try {
+                $this->checkPimcoreRequirements();
+            } catch (\Exception $e) {
+                return $this->handleException($e, 3);
             }
+        }
+
+        try {
+            $this->checkFilesystemPrerequisites();
+        } catch (\RuntimeException $e) {
+            return $this->handleException($e, 4);
+        }
+
+        return 0;
+    }
+
+    /**
+     * @param array ...$parts
+     *
+     * @return string
+     */
+    private function path(...$parts)
+    {
+        return $this->path . DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR, $parts);
+    }
+
+    private function backupFiles()
+    {
+        $fs = $this->fs;
+
+        $fs->mkdir($this->path('migration-backup'));
+
+        $dirs = ['app', 'bin', 'src', 'web', 'var', 'pimcore'];
+        foreach ($dirs as $dir) {
+            $source = $this->path($dir);
+            $target = $this->path('migration-backup', $dir);
+
+            if ($fs->exists($source)) {
+                $fs->rename($source, $target);
+            }
+        }
+
+        $files = ['index.php', '.htaccess'. 'composer.json', 'composer.lock'];
+        foreach ($files as $file) {
+            $source = $this->path($file);
+            $target = $this->path('migration-backup', $file);
+
+            if ($fs->exists($source)) {
+                $fs->rename($source, $target);
+            }
+        }
+
+        $websiteSource = $this->path('website');
+        $websiteTarget = $this->path('website');
+
+        if ($fs->exists($websiteSource)) {
+            $fs->mkdir($this->path('legacy'));
+            $fs->rename($websiteSource, $websiteTarget);
         }
     }
 
-    private function checkVersionPrerequisites(PimcoreStyle $io, VersionReader $versionReader)
+    private function checkFilesystemPrerequisites()
+    {
+        if (!$this->fs->exists($this->path('website'))) {
+            throw new \RuntimeException('Website directory found in ' . $this->path('website'));
+        }
+
+        if ($this->fs->exists($this->path('legacy'))) {
+            throw new \RuntimeException('Legacy directory already exists');
+        }
+
+        if ($this->fs->exists($this->path('migration-backup'))) {
+            throw new \RuntimeException('migration-backup directory already exists');
+        }
+    }
+
+    /**
+     * Test if version prerequisites match (Pimcore >= 4.5.0, < 5)
+     *
+     * @param VersionReader $versionReader
+     */
+    private function checkVersionPrerequisites(VersionReader $versionReader)
     {
         $version = $versionReader->getVersion();
 
@@ -114,20 +189,53 @@ class MigrateFilesystemCommand extends Command
         if (version_compare($version, '4.5', '<')) {
             throw new \RuntimeException(sprintf('Please update to version 4.5.0 before upgrading to version 5', $version));
         }
+
+        $this->io->success('Pimcore version prerequisites match');
     }
 
-    private function checkPimcoreRequirements(PimcoreStyle $io): bool
+    /**
+     * Test if Pimcore 5 requirements match
+     *
+     * @return bool
+     */
+    private function checkPimcoreRequirements()
     {
-        $io->text('');
-        $io->comment('Checking Pimcore 5 requirements');
+        $this->io->text('');
+        $this->io->comment('Checking Pimcore 5 requirements');
 
-        $formatter = new RequirementsFormatter($io);
+        $formatter = new RequirementsFormatter($this->io);
 
-        return $formatter->checkRequirements(new Pimcore5Requirements());
+        if (!$formatter->checkRequirements(new Pimcore5Requirements())) {
+            throw new \RuntimeException('Pimcore 5 requirements check failed');
+        }
     }
 
-    private function isDryRun(InputInterface $input): bool
+    private function isDryRun(): bool
     {
-        return (bool)$input->getOption('dry-run');
+        return (bool)$this->io->getInput()->getOption('dry-run');
+    }
+
+    /**
+     * Prints exception message as error and resolves exit code
+     *
+     * @param \Exception $e
+     * @param int $defaultExitCode
+     *
+     * @return int
+     */
+    private function handleException(\Exception $e, int $defaultExitCode = 1): int
+    {
+        if ($defaultExitCode <= 0) {
+            throw new \InvalidArgumentException('Default return code must be >= 0');
+        }
+
+        $this->io->error($e->getMessage());
+
+        $exitCode = (int)$e->getCode();
+        if ($exitCode > 0) {
+            return $exitCode;
+        }
+
+        return $defaultExitCode;
     }
 }
